27f820912364779af04ac5e6ff080b7ad2848704
Niclas Mølby <niclas@molby.net>
2019-08-15 13:13:06 +0200

    Changed home directory

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 user="ubuntu"
-targetfolder="/home"
+targetfolder="/home/ubuntu"
 declare -A ips=( ["local"]="104.155.53.172" ["staging"]="34.76.103.52" ["prod"]="34.76.115.57")
 if [ "$1" != "" ] && [ "${ips["$1"]}" != "" ]
 then
     ip="${ips["$1"]}"
     scp docker-compose.yml $user@$ip:$targetfolder
-    ssh $user@$ip "
+    ssh $user@$ip ""
         cd $targetfolder
         docker-compose up
     "
